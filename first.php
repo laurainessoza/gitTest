c53088e1e37b5aea1e6ebf45da0312869759a8da
lauraine <laurainesozao@gmail.com>
2022-10-25 11:51:23 +0200

    commiting all files to the repository

---
+++ first.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        Getting started with git
+    </head>
+    <style>
+        body {
+            background-image: url('pics/animal.jpeg');
+            background-color: #C82828;  
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        </style>
+    <body>
+    
+    <form action="" method="post" name="Login_Form">
+  <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+    <?php if(isset($msg)){?>
+    <tr>
+      <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+    </tr>
+    <?php } ?>
+    <tr>
+      <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
+    </tr>
+    <tr>
+      <td align="right" valign="top">Username</td>
+      <td><input name="Username" type="text" class="Input"></td>
+    </tr>
+    <tr>
+      <td align="right">Password</td>
+      <td><input name="Password" type="password" class="Input"></td>
+    </tr>
+    <tr>
+      <td> </td>
+      <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+    </tr>
+    <p> Animals also provide a number of resources that humans rely on, such as food, fur, and leather. In some cases, animals are even used in research to help develop new medicines and treatments for diseases. Despite the important role that animals play in our world, many people do not value them as they should.</p><br>
+    <h3>FOOD</h3>
+        <p> Food is the substance we eat every day for energy and strength. There are many different types of food, such as fruits, vegetables, rice, and pasta. We need to eat a variety of foods to get all the essential nutrients the body needs. Not eating a healthy and balanced diet leads to weakness and deficiency diseases.</p>
+
+    </body>
+
+</html>
+
